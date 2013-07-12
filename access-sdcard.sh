fc704e4edee76c96ebb98025e8df8fed819caf80
Michael Weber <michael@xmw.de>
2013-07-12 07:54:39 +0200

    Preparations for qemu support. Signed-off-by: Michael Weber <michael@xmw.de>

--- access-sdcard.sh
+++ access-sdcard.sh
@@ -2,6 +2,7 @@
 # Michael Weber xmw at gentoo dot org 2013
 # vim: tabstop=4
 
+RUN_QEMU=0
 
 usage() {
 	echo "Usage: $(basename ${0}) [-v] [-h|--help] <image> [mountpoint]"
@@ -13,15 +14,27 @@ while true ; do
 		-h|--help)
 			usage 0
 			;;
-		-v)
+		-v|--verbose)
 			set +x
 			shift
 			;;
+#		-r|--run)
+#			RUN_QEMU=1
+#			shift
+#			;;
 		*)
-			break 2
+			break
 			;;
 	esac
 done
+
+if [ "${RUN_QEMU}" -eq 1 ] ; then
+	if ! qemu-system-arm -version || \
+		! qemu-system-arm -cpu ? | grep arm1176 >/dev/null ; then
+		echo "Please install qemu-system-arm with arm1176 support." 
+		exit 1;
+	fi
+fi
 
 IMAGE=${1}
 MOUNTPOINT=${2}
@@ -43,20 +56,32 @@ LOOP=$(losetup -f)
 losetup "${LOOP}" "${IMAGE}"
 partx -d "${LOOP}" || true
 partx -a "${LOOP}"
-mount "${LOOP}"p3 "${MOUNTPOINT}"
-mount "${LOOP}"p1 "${MOUNTPOINT}"/boot
 
-pushd "${MOUNTPOINT}"
-$SHELL
-popd >/dev/null
+if [ "${RUN_QEMU}" -eq 1 ] ; then
+	mount "${LOOP}"p1 "${MOUNTPOINT}"
+	pushd "${MOUNTPOINT}"
+	echo "fix me"
+	popd >/dev/null
+else 
+	mount "${LOOP}"p3 "${MOUNTPOINT}"
+	mount "${LOOP}"p1 "${MOUNTPOINT}"/boot
+	mount "${MOUNTPOINT}"/var/cache/portage.squashfs "${MOUNTPOINT}"/usr/portage
+	pushd "${MOUNTPOINT}"
+	$SHELL
+	popd >/dev/null
 
-while mountpoint "${MOUNTPOINT}"/boot >/dev/null ; do
-	umount "${MOUNTPOINT}"/boot || sleep 1
-done
+	while mountpoint "${MOUNTPOINT}"/usr/portage >/dev/null ; do
+		umount "${MOUNTPOINT}"/usr/portage || sleep 1
+	done
+	while mountpoint "${MOUNTPOINT}"/boot >/dev/null ; do
+		umount "${MOUNTPOINT}"/boot || sleep 1
+	done
+fi
+
 while mountpoint "${MOUNTPOINT}" >/dev/null ; do
 	umount "${MOUNTPOINT}" || sleep 1
 done
-partx -d "${LOOP}" || true
+
 while losetup -a | grep "${IMAGE}" | grep "${LOOP}" >/dev/null ; do
 	losetup -d "${LOOP}" || sleep 1
 done
