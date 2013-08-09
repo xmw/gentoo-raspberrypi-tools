1ca2ada27dff1f2ca26cd13c390b8d55daed18c9
Michael Weber <michael@xmw.de>
2013-08-09 15:38:43 +0200

    binhost helper scripts

---
+++ binhost-pull.sh
@@ -0,0 +1,3 @@
+#!bin/sh
+
+rsync --bwlimit=80 -avP --chmod=a+rX --no-owner --no-group lore.xmw.de:/srv/gentoo/genberry/experimental/ /var/cache/packages/
---
+++ binhost-push.sh
@@ -0,0 +1,3 @@
+#!bin/sh
+
+rsync --bwlimit=80 -avPL --delete --chmod=a+rX --no-owner --no-group /var/cache/packages/ lore.xmw.de:/srv/gentoo/genberry/experimental/
