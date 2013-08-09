4747c642d99480175d51d532aaf5476009f19404
Michael Weber <michael@xmw.de>
2013-08-09 15:40:08 +0200

    fix interpreter

--- binhost-pull.sh
+++ binhost-pull.sh
@@ -1,3 +1,3 @@
-#!bin/sh
+#!/bin/sh
 
 rsync --bwlimit=80 -avP --chmod=a+rX --no-owner --no-group lore.xmw.de:/srv/gentoo/genberry/experimental/ /var/cache/packages/
--- binhost-push.sh
+++ binhost-push.sh
@@ -1,3 +1,3 @@
-#!bin/sh
+#!/bin/sh
 
 rsync --bwlimit=80 -avPL --delete --chmod=a+rX --no-owner --no-group /var/cache/packages/ lore.xmw.de:/srv/gentoo/genberry/experimental/
