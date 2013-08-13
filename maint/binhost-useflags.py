4659dc3698ac73698c9e749743447e7cffb146b7
Michael Weber <michael@xmw.de>
2013-08-13 19:18:57 +0200

    python fix.

--- maint/binhost-useflags.py
+++ maint/binhost-useflags.py
@@ -15,9 +15,9 @@ for i in bh._pkgindex.packages:
     iuses = i.get('IUSE', '').split()
     if not iuses:
         continue
-    for i in len(iuses):
-        if iuses[i][0] in ('+', '-'):
-            iuses[i] = iuses[i][1:]
+    for j in range(len(iuses)):
+        if iuses[j][0] in ('+', '-'):
+            iuses[j] = iuses[j][1:]
     uses = i.get('USE', '').split()
     flags = map(lambda iuse: (not iuse in uses and '-' or '' ) + iuse, iuses)
     lines.append((i.get('BUILD_TIME'), '=%s %s' % (i.get('CPV'), ' '.join(flags))))
